ffb2ea513b2d7de2bd89c2770ef3bc261e0c69b0
Neby <Neby@NEBY-PC>
2017-01-18 11:48:16 +0800

    1

--- WindAddin/Z/UserControl_InsertCoordinate.Designer.cs
+++ WindAddin/Z/UserControl_InsertCoordinate.Designer.cs
@@ -28,171 +28,271 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserControl_InsertCoordinate));
+            this.xtraTabControl1 = new DevExpress.XtraTab.XtraTabControl();
+            this.xtraTabPage4 = new DevExpress.XtraTab.XtraTabPage();
             this.txtZ1 = new System.Windows.Forms.TextBox();
+            this.btnInsertCoordinate_One = new DevExpress.XtraEditors.SimpleButton();
             this.label1 = new System.Windows.Forms.Label();
             this.txtZ2 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtX1 = new System.Windows.Forms.TextBox();
             this.txtY1 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtX2 = new System.Windows.Forms.TextBox();
             this.txtY2 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.txtX2 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.txtX1 = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.btnInsertCoordinate = new DevExpress.XtraEditors.SimpleButton();
+            this.xtraTabPage2 = new DevExpress.XtraTab.XtraTabPage();
+            this.xtraTabPage3 = new DevExpress.XtraTab.XtraTabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.xtraTabControl1)).BeginInit();
+            this.xtraTabControl1.SuspendLayout();
+            this.xtraTabPage4.SuspendLayout();
+            this.xtraTabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // xtraTabControl1
+            // 
+            this.xtraTabControl1.Appearance.BackColor = System.Drawing.Color.Transparent;
+            this.xtraTabControl1.Appearance.BackColor2 = System.Drawing.Color.Transparent;
+            this.xtraTabControl1.Appearance.BorderColor = System.Drawing.Color.Transparent;
+            this.xtraTabControl1.Appearance.Options.UseBackColor = true;
+            this.xtraTabControl1.Appearance.Options.UseBorderColor = true;
+            this.xtraTabControl1.AppearancePage.HeaderActive.BackColor = System.Drawing.Color.Transparent;
+            this.xtraTabControl1.AppearancePage.HeaderActive.BorderColor = System.Drawing.Color.Transparent;
+            this.xtraTabControl1.AppearancePage.HeaderActive.Options.UseBackColor = true;
+            this.xtraTabControl1.AppearancePage.HeaderActive.Options.UseBorderColor = true;
+            this.xtraTabControl1.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.xtraTabControl1.BorderStylePage = DevExpress.XtraEditors.Controls.BorderStyles.HotFlat;
+            this.xtraTabControl1.Location = new System.Drawing.Point(12, 24);
+            this.xtraTabControl1.Name = "xtraTabControl1";
+            this.xtraTabControl1.PaintStyleName = "Standard";
+            this.xtraTabControl1.SelectedTabPage = this.xtraTabPage4;
+            this.xtraTabControl1.Size = new System.Drawing.Size(220, 301);
+            this.xtraTabControl1.TabIndex = 3;
+            this.xtraTabControl1.TabPages.AddRange(new DevExpress.XtraTab.XtraTabPage[] {
+            this.xtraTabPage4,
+            this.xtraTabPage2,
+            this.xtraTabPage3});
+            // 
+            // xtraTabPage4
+            // 
+            this.xtraTabPage4.Controls.Add(this.txtZ1);
+            this.xtraTabPage4.Controls.Add(this.btnInsertCoordinate_One);
+            this.xtraTabPage4.Controls.Add(this.pictureBox1);
+            this.xtraTabPage4.Controls.Add(this.label4);
+            this.xtraTabPage4.Controls.Add(this.label1);
+            this.xtraTabPage4.Controls.Add(this.txtY2);
+            this.xtraTabPage4.Controls.Add(this.txtZ2);
+            this.xtraTabPage4.Controls.Add(this.txtX2);
+            this.xtraTabPage4.Controls.Add(this.label5);
+            this.xtraTabPage4.Controls.Add(this.label2);
+            this.xtraTabPage4.Controls.Add(this.label6);
+            this.xtraTabPage4.Controls.Add(this.label3);
+            this.xtraTabPage4.Controls.Add(this.txtX1);
+            this.xtraTabPage4.Controls.Add(this.txtY1);
+            this.xtraTabPage4.Name = "xtraTabPage4";
+            this.xtraTabPage4.Size = new System.Drawing.Size(216, 276);
+            this.xtraTabPage4.Text = "单个输入";
             // 
             // txtZ1
             // 
-            this.txtZ1.Location = new System.Drawing.Point(42, 63);
+            this.txtZ1.Location = new System.Drawing.Point(35, 100);
             this.txtZ1.Name = "txtZ1";
-            this.txtZ1.Size = new System.Drawing.Size(57, 21);
-            this.txtZ1.TabIndex = 0;
+            this.txtZ1.Size = new System.Drawing.Size(48, 22);
+            this.txtZ1.TabIndex = 4;
+            // 
+            // btnInsertCoordinate_One
+            // 
+            this.btnInsertCoordinate_One.Location = new System.Drawing.Point(68, 232);
+            this.btnInsertCoordinate_One.Name = "btnInsertCoordinate_One";
+            this.btnInsertCoordinate_One.ShowFocusRectangle = DevExpress.Utils.DefaultBoolean.False;
+            this.btnInsertCoordinate_One.Size = new System.Drawing.Size(75, 28);
+            this.btnInsertCoordinate_One.TabIndex = 16;
+            this.btnInsertCoordinate_One.Text = "插  入";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(19, 66);
+            this.label1.Location = new System.Drawing.Point(12, 103);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(22, 17);
-            this.label1.TabIndex = 1;
+            this.label1.TabIndex = 15;
             this.label1.Text = "Z1";
             // 
             // txtZ2
             // 
-            this.txtZ2.Location = new System.Drawing.Point(137, 63);
+            this.txtZ2.Location = new System.Drawing.Point(35, 176);
             this.txtZ2.Name = "txtZ2";
-            this.txtZ2.Size = new System.Drawing.Size(57, 21);
-            this.txtZ2.TabIndex = 0;
+            this.txtZ2.Size = new System.Drawing.Size(48, 22);
+            this.txtZ2.TabIndex = 5;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(114, 66);
+            this.label2.Location = new System.Drawing.Point(12, 179);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(22, 17);
-            this.label2.TabIndex = 1;
+            this.label2.TabIndex = 14;
             this.label2.Text = "Z2";
-            // 
-            // txtY1
-            // 
-            this.txtY1.Location = new System.Drawing.Point(137, 99);
-            this.txtY1.Name = "txtY1";
-            this.txtY1.Size = new System.Drawing.Size(57, 21);
-            this.txtY1.TabIndex = 0;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label3.Location = new System.Drawing.Point(114, 102);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(22, 17);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "Y1";
-            // 
-            // txtY2
-            // 
-            this.txtY2.Location = new System.Drawing.Point(137, 135);
-            this.txtY2.Name = "txtY2";
-            this.txtY2.Size = new System.Drawing.Size(57, 21);
-            this.txtY2.TabIndex = 0;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label4.Location = new System.Drawing.Point(114, 138);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(22, 17);
-            this.label4.TabIndex = 1;
-            this.label4.Text = "Y2";
-            // 
-            // txtX2
-            // 
-            this.txtX2.Location = new System.Drawing.Point(42, 135);
-            this.txtX2.Name = "txtX2";
-            this.txtX2.Size = new System.Drawing.Size(57, 21);
-            this.txtX2.TabIndex = 0;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label5.Location = new System.Drawing.Point(19, 138);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(23, 17);
-            this.label5.TabIndex = 1;
-            this.label5.Text = "X2";
-            // 
-            // txtX1
-            // 
-            this.txtX1.Location = new System.Drawing.Point(42, 99);
-            this.txtX1.Name = "txtX1";
-            this.txtX1.Size = new System.Drawing.Size(57, 21);
-            this.txtX1.TabIndex = 0;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label6.Location = new System.Drawing.Point(19, 102);
+            this.label6.Location = new System.Drawing.Point(40, 37);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(23, 17);
-            this.label6.TabIndex = 1;
+            this.label6.TabIndex = 10;
             this.label6.Text = "X1";
             // 
-            // btnInsertCoordinate
+            // txtX1
             // 
-            this.btnInsertCoordinate.Location = new System.Drawing.Point(68, 219);
-            this.btnInsertCoordinate.Name = "btnInsertCoordinate";
-            this.btnInsertCoordinate.ShowFocusRectangle = DevExpress.Utils.DefaultBoolean.False;
-            this.btnInsertCoordinate.Size = new System.Drawing.Size(75, 23);
-            this.btnInsertCoordinate.TabIndex = 2;
-            this.btnInsertCoordinate.Text = "插入坐标";
-            this.btnInsertCoordinate.Click += new System.EventHandler(this.btnInsertCoordinate_Click);
+            this.txtX1.Location = new System.Drawing.Point(63, 34);
+            this.txtX1.Name = "txtX1";
+            this.txtX1.Size = new System.Drawing.Size(52, 22);
+            this.txtX1.TabIndex = 9;
+            // 
+            // txtY1
+            // 
+            this.txtY1.Location = new System.Drawing.Point(63, 62);
+            this.txtY1.Name = "txtY1";
+            this.txtY1.Size = new System.Drawing.Size(52, 22);
+            this.txtY1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(40, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 17);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Y1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(137, 37);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "X2";
+            // 
+            // txtX2
+            // 
+            this.txtX2.Location = new System.Drawing.Point(160, 34);
+            this.txtX2.Name = "txtX2";
+            this.txtX2.Size = new System.Drawing.Size(52, 22);
+            this.txtX2.TabIndex = 8;
+            // 
+            // txtY2
+            // 
+            this.txtY2.Location = new System.Drawing.Point(160, 65);
+            this.txtY2.Name = "txtY2";
+            this.txtY2.Size = new System.Drawing.Size(52, 22);
+            this.txtY2.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(137, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Y2";
+            // 
+            // xtraTabPage2
+            // 
+            this.xtraTabPage2.Controls.Add(this.simpleButton1);
+            this.xtraTabPage2.Controls.Add(this.pictureBox2);
+            this.xtraTabPage2.Name = "xtraTabPage2";
+            this.xtraTabPage2.Size = new System.Drawing.Size(216, 276);
+            this.xtraTabPage2.Text = "连续输入";
+            // 
+            // xtraTabPage3
+            // 
+            this.xtraTabPage3.Name = "xtraTabPage3";
+            this.xtraTabPage3.Size = new System.Drawing.Size(216, 203);
+            this.xtraTabPage3.Text = "等分输入";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(99, 93);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(83, 105);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(21, 58);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(166, 162);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(69, 233);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.ShowFocusRectangle = DevExpress.Utils.DefaultBoolean.False;
+            this.simpleButton1.Size = new System.Drawing.Size(75, 28);
+            this.simpleButton1.TabIndex = 17;
+            this.simpleButton1.Text = "插  入";
             // 
             // UserControl_InsertCoordinate
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.btnInsertCoordinate);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtX1);
-            this.Controls.Add(this.txtX2);
-            this.Controls.Add(this.txtY2);
-            this.Controls.Add(this.txtY1);
-            this.Controls.Add(this.txtZ2);
-            this.Controls.Add(this.txtZ1);
+            this.Controls.Add(this.xtraTabControl1);
             this.Name = "UserControl_InsertCoordinate";
-            this.Size = new System.Drawing.Size(245, 408);
+            this.Size = new System.Drawing.Size(245, 574);
+            ((System.ComponentModel.ISupportInitialize)(this.xtraTabControl1)).EndInit();
+            this.xtraTabControl1.ResumeLayout(false);
+            this.xtraTabPage4.ResumeLayout(false);
+            this.xtraTabPage4.PerformLayout();
+            this.xtraTabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
+        private DevExpress.XtraTab.XtraTabControl xtraTabControl1;
+        private DevExpress.XtraTab.XtraTabPage xtraTabPage2;
+        private DevExpress.XtraTab.XtraTabPage xtraTabPage3;
+        private DevExpress.XtraTab.XtraTabPage xtraTabPage4;
         private System.Windows.Forms.TextBox txtZ1;
+        private DevExpress.XtraEditors.SimpleButton btnInsertCoordinate_One;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtZ2;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtX1;
         private System.Windows.Forms.TextBox txtY1;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtX2;
         private System.Windows.Forms.TextBox txtY2;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox txtX2;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox txtX1;
-        private System.Windows.Forms.Label label6;
-        private DevExpress.XtraEditors.SimpleButton btnInsertCoordinate;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
     }
 }
