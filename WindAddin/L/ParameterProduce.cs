d71ac0358c3986cc60e089f69fa005b5084e10c6
LXY <liuxinyan0428@gmail.com>
2017-01-18 10:16:23 +0800

    加入了单个和连续的单元格写入方法

--- WindAddin/L/ParameterProduce.cs
+++ WindAddin/L/ParameterProduce.cs
@@ -7,12 +7,36 @@ namespace WindAddin
 {
     class ParameterProduce
     {
-        public  ParameterProduce()
+        public string OneWrite(params string[] para)
         {
-            /////////////////////////////////
+
+            return OneGenerateRec(para);
+
         }
 
-        public string OneGenerateRec(params string[] para)
+        public string[,] ContinuousWrite(string[] X,string[] Y,string[] Z,string precision)
+        {
+            string[,] para = new string[X.Length - 1, Z.Length - 1];
+            for (int i1 = 0; i1 < X.Length-1; i1++)
+            {
+                for (int i2 = 0; i2 < Z.Length-1; i2++)
+                {
+                    if (precision == null)
+                    {
+                        para[i1, i2] = OneGenerateRec(Z[i2], Z[i2 + 1], X[i1], Y[i1], X[i1+1], Y[i1+1]);
+                    }
+                    else if (precision != null)
+                    {
+                        para[i1, i2] = OneGenerateRec(Z[i2], Z[i2 + 1], X[i1], Y[i1], X[i1 + 1], Y[i1 + 1],precision);
+                    }
+                }
+            }
+            return para;
+        }
+
+
+
+        private string OneGenerateRec(params string[] para)
         {
             string cellPara = "";
             if (para.Length == 6)       //应该是动态数组改为count
