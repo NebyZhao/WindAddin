17a93bc4db8694c066dbe3ab77e76ba21ced8e5f
LXY <liuxinyan0428@gmail.com>
2017-01-19 00:31:32 +0800

    返还了angle 数组

--- WindAddin/L/AllInOne.cs
+++ WindAddin/L/AllInOne.cs
@@ -14,13 +14,14 @@ namespace WindAddin.L
         public AllInOne(Microsoft.Office.Interop.Excel.Workbook xlWb,Microsoft.Office.Interop.Excel.Worksheet xlWs,string[] files)
         {
             this.files = files;
+            angle = new string[files.Length];
 
-
-            for (int i = 0; i < files.Length; i++)
+            for (int i1 = 0; i1 < files.Length; i1++)
             {
-                DataFile DaF = new DataFile(files[i]);
+                DataFile DaF = new DataFile(files[i1]);
                 FuckExcel FE = new FuckExcel(DaF, xlWb, xlWs);
                 FE.ManageExcel();
+                angle[i1] = DaF.angle.ToString();
             }
 
 
